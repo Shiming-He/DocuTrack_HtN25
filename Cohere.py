9adc8dd9f411244d4633c5bb0ab18f1698d72cc6
Aaron Xu <121455537+AaronXu07@users.noreply.github.com>
2025-09-13 10:28:39 -0400

    Added image reading function to Cohere.py

--- Cohere.py
+++ Cohere.py
@@ -1,7 +1,41 @@
 import cohere
+import base64
 
 # Initialize Cohere client
-co = cohere.Client("rja3QCQXw1WiOJhOt6Xm5qfgRYSRiRgAc91d3v6M")
+co = cohere.Client("g7RUPaPLEX9HWzrORaU1PJkTZHSVRx0Uu8eOVLPY")
+
+def validate_latex(latex_code: str) -> str:
+    if "\\begin{document}" not in latex_code:
+        latex_code = "\\begin{document}\n" + latex_code
+    if "\\end{document}" not in latex_code:
+        latex_code += "\n\\end{document}"
+    return latex_code
+
+def generate_text(image_path, message):
+    model = "c4ai-aya-vision-8b"
+    co = cohere.ClientV2("g7RUPaPLEX9HWzrORaU1PJkTZHSVRx0Uu8eOVLPY")
+
+    with open(image_path, "rb") as img_file:
+        base64_image_url = f"data:image/jpeg;base64,{base64.b64encode(img_file.read()).decode('utf-8')}"
+
+    response = co.chat(
+        model=model,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": message},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": base64_image_url},
+                    },
+                ],
+            }
+        ],
+        temperature=0.3,
+    )
+
+    print(response.message.content[0].text)
 
 
 # Example sequence of keystrokes (you can change this to test)
@@ -9,6 +43,7 @@ actions = [
     "Open browser",
     "Move cursor to email field",
     "Type: sampleemail@gmail.com",
+    
     "Press ENTER",
     "Type: samplepassword123",
     "Move cursor to login button",
@@ -19,15 +54,6 @@ actions = [
 
 # Join into a single string
 action_sequence = ", ".join(actions)
-
-
-def validate_latex(latex_code: str) -> str:
-    if "\\begin{document}" not in latex_code:
-        latex_code = "\\begin{document}\n" + latex_code
-    if "\\end{document}" not in latex_code:
-        latex_code += "\n\\end{document}"
-    return latex_code
-
 
 # Ask Cohere to output latex documentation
 message = f"""
